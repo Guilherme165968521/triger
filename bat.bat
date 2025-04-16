60b6b1d22e7d9ffcd5b337d6046fe0cd0e4bea5e
Guilherme165968521 <guilnhermedesouzaferreira8@gmail.com>
2025-04-16 18:02:00 -0300

    first commit

---
+++ bat.bat
@@ -0,0 +1,7 @@
+echo "# trigger" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Guilherme165968521/triger.git
+git push -u origin main
